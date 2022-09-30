5420547bc88f6064e1929d5d181dc2ab7554d253
salina sapkota <ssapkota8@my.centennialcollege.ca>
2022-09-30 08:38:02 -0400

    debbuger added

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import session from 'express-session';
+import debug from 'debug';
+debug('comp-229');
+import http from 'http';
 
-// ES Modules fix for __dirname
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import app from './app/app.js';
 
-// Configuration Module
-import { Secret } from './config/config.js';
+const PORT = normalizePort(process.env.PORT || 2000);
+app.set('port', PORT);
 
-// Import Routes
-import indexRouter from './app/routes/index.route.server.js'
+const server = http.createServer(app);
 
-// Instantiate Express Application
-const app = express();
+server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
 
-// Set Up Middlewares
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
 
-// Setup ViewEngine EJS
-app.set('views', path.join(__dirname, '/app/views'));
-app.set('view engine', 'ejs');
+    if (port >= 0) {
+        return port;
+    }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname,'/client')));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use(session({
-    secret: Secret,
-    saveUninitialized: false, 
-    resave: false
-}));
+    return false;
+}
 
-// Use Routes
-app.use('/', indexRouter);
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-app.listen(2000);
+    let bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening() 
+{
+  let addr = server.address();
+  let bind = 'pipe ' + addr;
+  debug('Listening on ' + bind);
+}
