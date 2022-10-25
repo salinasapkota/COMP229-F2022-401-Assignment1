5ec38c89a7eb1cdfccd86adaa975c9043f657926
salina sapkota <ssapkota8@my.centennialcollege.ca>
2022-10-24 22:12:13 -0400

    added cookie-parser

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import http from 'http';
 
 import app from './app/app.js';
 
-const PORT = normalizePort(process.env.PORT || 2000);
+const PORT = normalizePort(process.env.PORT || 3000);
 app.set('port', PORT);
 
 const server = http.createServer(app);
